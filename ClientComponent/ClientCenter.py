f7b59f17a78cd0d21c1166576da2eabaab49c794
yyx <273839986@qq.com>
2024-03-06 21:22:47 +0800

    改成心跳模式

--- ClientComponent/ClientCenter.py
+++ ClientComponent/ClientCenter.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import time
 
 # 服务器地址和端口
 server_address = '103.40.13.95'  # 云服务器的IP地址
@@ -7,19 +9,40 @@ server_port = 59748                 # 服务器端口号
 # 创建一个 TCP/IP 套接字
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+def send_heartbeat():
+    while True:
+        try:
+            # 发送心跳消息
+            client_socket.sendall(b'heartbeat')
+            time.sleep(10)  # 每分钟发送一次心跳消息
+        except:
+            break
+
+
+def receive_heartbeat():
+    while True:
+        try:
+            # 接收心跳消息
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print('Received heartbeat from server:', data.decode())
+        except:
+            break
+
+
 try:
     # 连接服务器
     client_socket.connect((server_address, server_port))
 
+    # 启动发送和接收心跳消息的线程
+    threading.Thread(target=send_heartbeat).start()
+    threading.Thread(target=receive_heartbeat).start()
+
     # 发送和接收数据
     while True:
-        # 发送数据
-        message = 'Hello from client!'
-        client_socket.sendall(message.encode())
-
-        # 接收数据
-        data = client_socket.recv(1024)
-        print('Received:', data.decode())
+        pass
 finally:
     # 关闭连接
     client_socket.close()
--- ServerComponent/ServerCenter.py
+++ ServerComponent/ServerCenter.py
@@ -1,5 +1,8 @@
 #这个用于建立链接，确保链接上了客户端
 import socket
+import threading
+import time
+
 
 # 监听地址和端口
 host = '0.0.0.0'  # 服务器IP地址，'0.0.0.0'表示监听所有可用的网络接口
@@ -15,21 +18,22 @@ server_socket.bind((host, port))
 server_socket.listen(1)
 
 print('Waiting for a connection...')
-connection, client_address = server_socket.accept()
 
-while True:
-    connection, client_address = server_socket.accept()
+def handle_connection(connection, client_address):
     try:
         print('Connection from', client_address)
 
         while True:
             data = connection.recv(1024)
-            if data:
-                print('Received:', data.decode())
-                connection.sendall(b'Hello from server!')
-            else:
+            if not data:
                 break
+            print('Received:', data.decode())
+            connection.sendall(b'Hello from server!')
     finally:
         # 关闭连接
         connection.close()
+
+while True:
+    connection, client_address = server_socket.accept()
+    threading.Thread(target=handle_connection, args=(connection, client_address)).start()
 
