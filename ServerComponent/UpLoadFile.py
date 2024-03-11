51893bef95591f151971a7c689968d42206dbd32
yyx <273839986@qq.com>
2024-03-11 14:08:24 +0800

    上传文件名字改为客户端文件名字

--- ServerComponent/UpLoadFile.py
+++ ServerComponent/UpLoadFile.py
@@ -1,11 +1,13 @@
-# 云服务器代码 cloud_server.py
-
 import http.server
 import os
 
 
 class FileUploadHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
+        # 获取文件名
+        content_disposition = self.headers.get('Content-Disposition')
+        filename = content_disposition.split(';')[1].split('=')[1].strip('"')
+
         content_length = int(self.headers['Content-Length'])
         # 从请求中读取文件内容
         file_content = self.rfile.read(content_length)
@@ -15,7 +17,7 @@ class FileUploadHandler(http.server.BaseHTTPRequestHandler):
         os.makedirs(upload_dir, exist_ok=True)
 
         # 保存上传的文件到指定目录
-        with open(os.path.join(upload_dir, 'uploaded_file.txt'), 'wb') as f:
+        with open(os.path.join(upload_dir, filename), 'wb') as f:
             f.write(file_content)
 
         # 返回上传成功的响应
@@ -30,4 +32,3 @@ def add_File():
     httpd = http.server.HTTPServer(server_address, FileUploadHandler)
     print('Starting server...')
     httpd.serve_forever()
-
