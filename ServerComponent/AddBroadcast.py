1a25a68ff615f1fdf0c40f333d04c5a67cc74c28
yyx0080 <273839986@qq.com>
2024-03-17 16:06:57 +0800

    1606解决多种格式问题1.0服务器

--- ServerComponent/AddBroadcast.py
+++ ServerComponent/AddBroadcast.py
@@ -8,25 +8,25 @@ channel = connection.channel()
 
 # 声明一个名为 'file_broadcast2' 的队列 湖北，一一对应
 # 同时，这个队列，包含上传文件，修改文件，删除文件等信息
-channel.queue_declare(queue='file_broadcast2')
+channel.queue_declare(queue='file_broadcast1')  # 宿舍电脑
+channel.queue_declare(queue='file_broadcast2')  # 湖北客户端
+
 
 # 定义文件上传函数
 # 这个函数后面增加一个标志位，标志哪一个客户端发送
 # 那么这个客户端就不用把数据送入消息队列中
 def add_broupload_file(file_path):
     filename = os.path.basename(file_path)
-    with open(file_path, 'rb') as file:
-        file_content = file.read()
-        # 使用 Base64 编码将字节类型转换为字符串类型
-        file_content_base64 = base64.b64encode(file_content).decode('utf-8')
-        # 组装消息
-        message = {
-            'filename': filename,
-            'file_content': file_content_base64
-        }
-        # 发送文件内容到队列
-        # 这里后面要多次调用，就是不用调用谁发送的消息队列，谁发送的，他的消息队列就不调用
-        channel.basic_publish(exchange='', routing_key='file_broadcast2', body=json.dumps(message))
-    print("File '{}' uploaded add_file_broadcas2t successfully.".format(filename))
+    # 组装消息
+    message = {
+        'filename': filename,
+        'from': 123
+    }
+    # 发送文件内容到队列
+    # 这里后面要多次调用，就是不用调用谁发送的消息队列，谁发送的，他的消息队列就不调用
+    channel.basic_publish(exchange='', routing_key='file_broadcast1', body=json.dumps(message))
+    channel.basic_publish(exchange='', routing_key='file_broadcast2', body=json.dumps(message))
+    print("File '{}' uploaded add_file_broadcast1 successfully.".format(filename))
+    print("File '{}' uploaded add_file_broadcast2 successfully.".format(filename))
 
 
--- ServerComponent/ShowFileContent.py
+++ ServerComponent/ShowFileContent.py
@@ -1,7 +1,7 @@
 # 展示File内容的网页
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for
 import os
-
+from urllib import request
 app = Flask(__name__)
 UPLOAD_FOLDER = 'C:/Users/Administrator/Desktop/File'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -17,5 +17,20 @@ def download_file(filename):
     # 从指定文件夹提供文件下载
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # 检查是否有文件被上传
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # 如果用户没有选择文件，浏览器会提交一个没有文件名的空文件
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            # 保存上传的文件到指定文件夹
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=25565,debug=True)
